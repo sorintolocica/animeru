2e65a1072b84def1378ebc6a417169826a66f32a
sorintolocica <sorinsagara@gmail.com>
2022-08-15 08:34:07 +0300

    v1

---
+++ templates/menu.php
@@ -0,0 +1,31 @@
+<div class="container">
+<header class="p-3 text-bg">
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <a class="navbar-brand" href="/animeru">
+                <div class="logo-image">
+                    <img src="https://3.bp.blogspot.com/-MvKE9gYa4FQ/X0BlAp7-_BI/AAAAAAAAA48/SOWftgfOF2Esij_TnEog5TyeXUdzFsJ0wCK4BGAYYCw/s1600/Untitled-1.png" class="img-fluid">
+                </div>
+            </a>
+
+
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 text-secondary">Acasa</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Lista anime</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Staff</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Despre noi</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Contacteaza-ne</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Cauta un anime..." aria-label="Search">
+            </form>
+
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Logare</button>
+                <button type="button" class="btn btn-warning">Inregistrare</button>
+            </div>
+        </div>
+    </div>
+</header>
+</div>
